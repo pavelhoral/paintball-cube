a9ce4c1252a68e4d29963e0555b2099997d3661e
Pavel Horal <pavel.horal@orchitech.cz>
2020-07-10 12:11:46 +0200

    Add better fail signal for cipher defuse.

--- src/modules/CipherDefuse.cpp
+++ src/modules/CipherDefuse.cpp
@@ -1,12 +1,16 @@
 #include "CipherDefuse.hpp"
 
+// HINT1 do re mi fa so la si do
+// HINT2 morseovka
+// HINT3 trezor
+
 #define STATE_DEFUSE 0
 #define STATE_HINT1 1
 #define STATE_HINT2 2
 #define STATE_HINT3 3
 #define STATE_WINNER 4
 
-#define DEFUSE_CODE 947
+#define DEFUSE_CODE 948
 
 #define ERROR_SOUND 1, 2
 #define WINNER_SOUND 1, 4
@@ -15,14 +19,14 @@
 
 const int SCALE_MELODY[] = {
   NOTE_00, NOTE_2(SCALE_TEMPO),
-  NOTE_C3, NOTE_2(SCALE_TEMPO),
-  NOTE_D3, NOTE_2(SCALE_TEMPO),
-  NOTE_E3, NOTE_2(SCALE_TEMPO),
-  NOTE_F3, NOTE_2(SCALE_TEMPO),
-  NOTE_G3, NOTE_2(SCALE_TEMPO),
-  NOTE_A3, NOTE_2(SCALE_TEMPO),
-  NOTE_B3, NOTE_2(SCALE_TEMPO),
-  NOTE_C4, NOTE_2(SCALE_TEMPO)
+  NOTE_C4, NOTE_2(SCALE_TEMPO),
+  NOTE_D4, NOTE_2(SCALE_TEMPO),
+  NOTE_E4, NOTE_2(SCALE_TEMPO),
+  NOTE_F4, NOTE_2(SCALE_TEMPO),
+  NOTE_G4, NOTE_2(SCALE_TEMPO),
+  NOTE_A4, NOTE_2(SCALE_TEMPO),
+  NOTE_B4, NOTE_2(SCALE_TEMPO),
+  NOTE_C5, NOTE_2(SCALE_TEMPO)
 };
 
 uint8_t scalePosition(uint8_t value) {
@@ -92,16 +96,16 @@ void CipherDefuse::loop() {
     case 0:
       handleDefuse();
       break;
-    case 1:
+    case STATE_HINT1:
       handleHint1();
       break;
-    case 2:
+    case STATE_HINT2:
       handleHint2();
       break;
-    case 3:
+    case STATE_HINT3:
       handleHint3();
       break;
-    case 4:
+    case STATE_WINNER:
       handleWinner();
       break;
   }  
@@ -127,8 +131,8 @@ void CipherDefuse::handleDefuse() {
       context_.audio.buzz(50);
       defuseCode_ *= 10;
     } else {
-      context_.audio.play(WINNER_SOUND);
       activeState_ = STATE_WINNER;
+      stateChange_ = true;
       return;
     }
   }
@@ -139,7 +143,7 @@ void CipherDefuse::handleDefuse() {
 
 void CipherDefuse::handleHint1() {
   if (hintState_ & 1) {
-    context_.display.showNumberDec(DEFUSE_CODE / 100);
+    context_.display.showNumberDec(DEFUSE_CODE / 100, false, 1, 1);
     context_.light.setLevels(0, 255, 0);
     melodyPlayer_.update(millis());
     return;
@@ -150,28 +154,36 @@ void CipherDefuse::handleHint1() {
     melodyPlayer_.play();
     melodyDecode_ = 0;
   }
-  if (millis() - errorTimer_ < 1000) {
+  if (millis() - errorTimer_ < 500) {
     context_.light.setLevels(255, 0, 0);
   } else {
-    context_.light.reset();
+    context_.light.setLevels(0, 0, 255);
   }
   melodyPlayer_.update(millis());
   uint8_t input = context_.input.readMeter();
+  if (input == 0 || input == 9) {
+    context_.display.reset();
+    return;
+  }
   uint8_t digit = scaleDigit(input);
-  uint8_t segments[] = { digit, digit, digit, digit };
-  context_.display.setSegments(segments, 4);
+  if (digit != 0) {
+    digit = context_.display.encodeDigit(input);
+  }
+  context_.display.setSegments(&digit, 1, 1);
   if (!context_.input.readSwitch()) {
     return; // no-input
   }
   errorTimer_ = 0;
-  uint8_t tone = scalePosition(context_.input.readMeter());
-  if (melodyDecode_ + 1 == tone) {
+  uint8_t toneIndex = scalePosition(context_.input.readMeter());
+  if (melodyDecode_ + 1 == toneIndex) {
     melodyDecode_++;
   } else {
-    errorTimer_ = millis();
-    melodyDecode_ = tone == 1 ? 1 : 0;
+    melodyDecode_ = 0;
+    if (toneIndex != 1) {
+      errorTimer_ = millis();
+    }
   }
-  melodyPlayer_.load(&SCALE_MELODY[tone * 2], 1);
+  melodyPlayer_.load(&SCALE_MELODY[toneIndex * 2], 1);
   melodyPlayer_.play();
   if (melodyDecode_ == 8) {
     context_.audio.play(WINNER_SOUND);
@@ -183,18 +195,17 @@ void CipherDefuse::handleHint2() {
   if (stateChange_ || context_.input.readSwitch()) {
     uint8_t dash = 0b01001001;
     context_.display.setSegments(&dash, 1, 2);
+    context_.audio.buzz(100);
+    delay(500);
     morseStart_ = millis();
     lastPosition_ = -1;
   }
-  const String code = "/ - . - . / - / . . / . - . / . . / /";
-  uint8_t position = (millis() - morseStart_) / 250;
-  if (position > 37) {
+  const String code = "- . - . / - / - . - - / . - . / . . / /";
+  uint8_t position = (millis() - morseStart_) / 500;
+  if (position > 39) {
     context_.light.reset();
     return;
-  } else if (position == 0) {
-    context_.audio.buzz(100);
-    delay(250);
-  } else if (position == 37) {
+  } else if (position == 39) {
     context_.audio.buzz(200);
   }
   if (lastPosition_ != position) {
@@ -217,34 +228,37 @@ void CipherDefuse::handleHint2() {
   }
 }
 
-const uint8_t HINT3_CODE[] = { 6, 4, 9, 2, 3, 1, 8 };
+const uint8_t HINT3_CODE[] = { 6, 4, 9, 3, 8, 1, 5 };
 
 void CipherDefuse::handleHint3() {
   if (hintState_ & 4) {
     context_.light.setLevels(0, 255, 0);
-    context_.display.showNumberDec(7);
+    context_.display.showNumberDec(DEFUSE_CODE % 10);
     melodyPlayer_.update(millis());
     return;
   }
   if (stateChange_) {
-    context_.light.setLevels(255, 0, 0);
+    context_.light.setLevels(0, 0, 255);
   }
   uint8_t value = context_.input.readMeter();
-  uint8_t digit = scaleDigit(value);
+  uint8_t digit = context_.display.encodeDigit(value);
   context_.display.setSegments(&digit, 1, 3);
 
   boolean clockwise = codePosition_ % 2 == 1;
   uint8_t target = HINT3_CODE[codePosition_];
 
   if (value == target) {
-    context_.audio.buzz(25);
-    codePosition_++;
-  } else if ((clockwise && lastValue_ < value) || (!clockwise && lastValue_ > value)) { 
     context_.audio.buzz(50);
+    codePosition_++;
+  } else if (((clockwise && lastValue_ < value) || (!clockwise && lastValue_ > value)) && codePosition_ != 0) {
+    context_.light.setLevels(255, 0, 0);
+    context_.audio.buzz(250);
+    context_.light.setLevels(0, 0, 255);
     codePosition_ = 0;
-  } else {
+  } else if (lastValue_ != value) {
     context_.audio.buzz(5);
   }
+  lastValue_ = value;
   if (codePosition_ >= 7) {
     context_.audio.play(WINNER_SOUND);
     hintState_ |= 4;
@@ -254,7 +268,8 @@ void CipherDefuse::handleHint3() {
 void CipherDefuse::handleWinner() {
   if (stateChange_) {
     context_.light.setLevels(0, 255, 0);
-    delay(500);
+    context_.display.showNumberDec(0, true);
     context_.audio.play(1, 1);
   }
+  stateChange_ = false;
 }
